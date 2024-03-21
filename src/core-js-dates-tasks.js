1f5728a9f3cd0c83d20aff8fac07cb62e4f7583d
Roman <mamontov.r1995@gmail.com>
2024-03-21 21:38:41 +0300

    feat: add getQuarter fn

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -217,8 +217,8 @@ function getNextFridayThe13th(date) {
  * Date(2024, 5, 1) => 2
  * Date(2024, 10, 10) => 4
  */
-function getQuarter(/* date */) {
-  throw new Error('Not implemented');
+function getQuarter(date) {
+  return Math.ceil((date.getUTCMonth() + 1) / 3);
 }
 
 /**
